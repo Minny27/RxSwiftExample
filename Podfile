bb806044f40cf40766afe5ce744da385955ed704
Minny27 <dltmdals0608@naver.com>
2022-08-24 22:57:37 +0900

    Feat: 로그인 화면 구현 - 아이디, 비밀번호 모두 6자리 이상일 때만 버튼 활성화 - rx 적용

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwiftExample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+
+  target 'RxSwiftExampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RxSwiftExampleUITests' do
+    # Pods for testing
+  end
+
+end
--- RxSwiftExample/ViewController.swift
+++ RxSwiftExample/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+class LoginViewModel {
+    let usernameTextfieldSubject = PublishSubject<String>()
+    let passwardTextfieldSubject = PublishSubject<String>()
+    
+    func isValid() -> Observable<Bool> {
+        return Observable.combineLatest(usernameTextfieldSubject.asObservable(), passwardTextfieldSubject.asObservable()).map { username, passward in
+            return username.count > 5 && passward.count > 5
+        }.startWith(false)
+    }
+}
 
 class ViewController: UIViewController {
-
+    private let loginViewModel = LoginViewModel()
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwardTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        print("tapped Login Button!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        usernameTextField.becomeFirstResponder()
+        
+        usernameTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.usernameTextfieldSubject).disposed(by: disposeBag)
+        passwardTextField.rx.text.map { $0 ?? ""}.bind(to: loginViewModel.passwardTextfieldSubject).disposed(by: disposeBag)
+        
+        loginViewModel.isValid().bind(to: loginButton.rx.isEnabled).disposed(by: disposeBag)
+        loginViewModel.isValid().map { $0 ? 1 : 0.1 }.bind(to: loginButton.rx.alpha).disposed(by: disposeBag)
     }
-
-
 }
 
